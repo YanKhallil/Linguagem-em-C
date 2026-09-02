9911700c0ccb3fa3c2a893f6b48430374ce82c9c
YanKhallil <yan.khallil@gmail.com>
2026-09-02 17:13:15 -0400

    Criar for_negativo_positivo_ate100.c

---
+++ for_negativo_positivo_ate100.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    //declaração de variáveis
+    double s = 0.0;
+
+    for(int i = 1;i <= 10;i++){
+        double termo = (double)1/(i*i);
+
+        if(i % 2 != 0){
+            s += termo;
+        }else{
+            s -= termo;
+        }
+    }   
+    printf("O valor da soma é: %.6lf\n", s);
+    return 0;
+}
