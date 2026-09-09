a432f6ef249e9a064c2a1b065ab053f64a82fe16
YanKhallil <yan.khallil@gmail.com>
2026-09-09 16:27:28 -0400

    Create for_menor_maior.c

---
+++ for_menor_maior.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int numero, maior, menor;
+
+    printf("Digite o 1o numero: ");
+    scanf("%d", &numero);
+
+    maior = numero;
+    menor = numero;
+
+    for (int i = 2; i <= 20; i++) {
+        printf("Digite o %do numero: ", i);
+        scanf("%d", &numero);
+
+        if (numero > maior) {
+            maior = numero;
+        }
+
+        if (numero < menor) {
+            menor = numero;
+        }
+    }
+
+    printf("\nMaior valor: %d\n", maior);
+    printf("Menor valor: %d\n", menor);
+
+    return 0;
+}
