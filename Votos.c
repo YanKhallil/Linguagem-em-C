9d2e486b46050d5030e4ef6ecbbb5e0c637ac5ae
YanKhallil <yan.khallil@gmail.com>
2026-09-14 18:09:51 -0400

    Update Votos.c

--- Votos.c
+++ Votos.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
+/*c) Em uma eleição presidencial, existem quatro candidatos. Os votos são informados
+através de código. Os dados utilizados para a escrutinagem obedecem a seguinte
+codificação:
+·       1,2,3,4 = voto para os respectivos candidatos;
+·       5 = voto nulo;
+·       6 = voto em branco.
+Elabore
+um programa que calcule e escreva:
+a) total de votos para cada candidato;
+b) total de votos nulos;
+c) total de votos em branco;
+d) percentual dos votos em branco e nulos sobre o total.
 
+Como finalizador do conjunto de votos, tem-se o valor 0.*/
 int main(){
     //declaração de variáveis
     int voto;
